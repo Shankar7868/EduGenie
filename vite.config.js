01c0fe1860c1da327bf0589849f81fb99a171d19
Shankar7868 <227003067@sastra.ac.in>
2026-06-29 00:27:13 +0530

    added vercel rewrite and vite proxy for AWS http webhook

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/webhook': {
+        target: 'http://18.61.252.168/webhook',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/webhook/, '')
+      }
+    }
+  }
 })
